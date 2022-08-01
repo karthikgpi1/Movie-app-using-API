d66006fbc494a92969971afded6f6a33b841bac8
karthikgpi1 <karthikgopi024@gmail.com>
2022-08-02 00:58:20 +0530

    all done

--- script.js
+++ script.js
@@ -28,15 +28,19 @@ function showMovies(data) {
     main.innerHTML = '';
         
     data.forEach(movie =>{
-        const{title, poster_path, vote_average, overview} = movie;
+        const{title, poster_path, vote_average, overview, release_date} = movie;
         const movieEl = document.createElement('div');
         movieEl.classList.add('movie');
         movieEl.innerHTML = `
         <img src="${IMG_URL+poster_path}" alt="${title}" />
 
         <div class="movie-info">
+        <div class="group">
           <h3>${title}</h3>
           <span class="${getcolor(vote_average)}">${vote_average}</span>
+        </div>
+
+        <h5>${release_date}</h5>
         </div>
 
         <div class="overview">
